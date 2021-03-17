1479fe8a1371d225c271769955096927b01d0282
LaunchDark <1134068151@qq.com>
2021-03-17 21:39:50 +0800

    添加新家具

--- Simulator/Assets/MyProject/Scripts/Instrument/其他/Door.cs
+++ Simulator/Assets/MyProject/Scripts/Instrument/其他/Door.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 门
+/// </summary>
 public class Door : Instrument
 {
 
     void Start()
     {
         type = InstrumentEnum.门;
+        CanScaleInstrument = false;
+
         MaxOffsetZ = 3f;
         MinOffsetZ = 0.5f;
-        canDropDis = 0f;
+        canDropDis = 3f;
+
+        width = 0.1f;
+        height = 2f;
+
         adsorbTypeList = new List<InstrumentEnum>();
         adsorbTypeList.Add(InstrumentEnum.墙);
     }
---
+++ Simulator/Assets/MyProject/Scripts/Instrument/饰品/Vase5Build.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 花瓶5
+/// </summary>
+public class Vase5Build : Instrument
+{
+    void Start()
+    {
+        type = InstrumentEnum.花瓶5;
+        isFreeInstrument = true;
+        CanScaleInstrument = true;
+
+        MaxOffsetZ = 1.5f;
+        MinOffsetZ = 0.1f;
+        canDropDis = 2f;
+
+        width = 0.1f;
+        height = 0.15f;
+    }
+
+}
